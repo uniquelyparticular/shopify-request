80621bb092414cc057f2a78a1d96bb2fa1779d1d
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-23 16:46:03 -0500

    fix: cause release

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,7 +3,6 @@ import { removeLeadingSlash } from './utils'
 describe('Shopify utils', () => {
   it('leading slash is removed', () => {
     const string = '/test'
-
     expect(removeLeadingSlash(string)).toEqual('test')
   })
 })
