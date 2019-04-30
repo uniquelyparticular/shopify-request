c1de6a1fbaa0b503eec271283c485a90993ec936
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-30 14:09:29 -0500

    feat: updated to add support for storefront API as well as admin

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
-import fetch from 'cross-fetch'
+import fetch from 'cross-fetch';
 
-import {
-  InitOptions,
-  Options,
-  Headers,
-  Fetch
-} from './types'
-import { removeLeadingSlash } from './utils'
+import { InitOptions, Options, Headers, Fetch } from './types';
+import { removeLeadingSlash } from './utils';
 
 export class createClient {
-  private access_token?: string
-  private client_key?: string
-  private client_pass?: string
-  private options?: Options
-  public fetch?: Fetch
+  private admin_access_token?: string;
+  private storefront_access_token?: string;
+  private client_key?: string;
+  private client_pass?: string;
+  private options?: Options;
+  public fetch?: Fetch;
 
   constructor(options: InitOptions) {
-    const { store_name, access_token, client_key, client_pass, ...others } = options
-    this.access_token = access_token
-    this.client_key = client_key
-    this.client_pass = client_pass
-    this.fetch = options.fetch ? options.fetch : fetch
+    const {
+      store_name,
+      admin_access_token,
+      storefront_access_token,
+      client_key,
+      client_pass,
+      ...others
+    } = options;
+    this.admin_access_token = admin_access_token;
+    this.storefront_access_token = storefront_access_token;
+    this.client_key = client_key;
+    this.client_pass = client_pass;
+    this.fetch = options.fetch ? options.fetch : fetch;
     this.options = {
       host: `${store_name}.myshopify.com`,
       ...others
-    }
+    };
   }
 
   async request(
@@ -34,49 +38,51 @@ export class createClient {
     requestHeaders: Headers = {}
   ) {
     const {
-      options: {
-        application,
-        host,
-        headers: classHeaders
-      }
-    } = this
+      options: { application, host, headers: classHeaders }
+    } = this;
 
-    const uri: string = `https://${host}/${removeLeadingSlash(path)}`
+    const uri: string = `https://${host}/${removeLeadingSlash(path)}`;
 
     const customHeaders = {
       ...classHeaders,
       ...requestHeaders
-    }
+    };
 
-    const headers: Headers = (this.access_token) ? {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.access_token}`,
-      'X-SHOPIFY-ACCESS-TOKEN': this.access_token,
-      'X-SHOPIFY-SDK-LANGUAGE': 'JS-REQUEST',
-      ...(application && { 'X-SHOPIFY-APPLICATION': application }),
-      ...customHeaders
-    } : {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${Buffer.from(`${this.client_key}:${this.client_pass}`).toString('base64')}`,
-      'api-key': this.client_key,
-      'X-API-KEY': this.client_key,
-      'X-SHOPIFY-SDK-LANGUAGE': 'JS-REQUEST',
-      ...(application && { 'X-SHOPIFY-APPLICATION': application }),
-      ...customHeaders
-    }
+    const headers: Headers = this.admin_access_token
+      ? {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.admin_access_token}`,
+          'X-SHOPIFY-ACCESS-TOKEN': this.admin_access_token,
+          'X-SHOPIFY-STOREFRONT-ACCESS-TOKEN': this.storefront_access_token,
+          'X-SHOPIFY-SDK-LANGUAGE': 'JS-REQUEST',
+          'X-SDK-VARIANT': 'javascript',
+          ...(application && { 'X-SHOPIFY-APPLICATION': application }),
+          ...customHeaders
+        }
+      : {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(
+            `${this.client_key}:${this.client_pass}`
+          ).toString('base64')}`,
+          'api-key': this.client_key,
+          'X-API-KEY': this.client_key,
+          'X-SHOPIFY-SDK-LANGUAGE': 'JS-REQUEST',
+          'X-SDK-VARIANT': 'javascript',
+          ...(application && { 'X-SHOPIFY-APPLICATION': application }),
+          ...customHeaders
+        };
 
     const body = customHeaders['Content-Type']
       ? data
-      : { body: JSON.stringify({ ...data }) }
+      : { body: JSON.stringify({ ...data }) };
 
     const response = await this.fetch(uri, {
       method,
       headers,
       ...(data && body)
-    })
+    });
 
-    if (response.status === 204)
-      return response.text()
+    if (response.status === 204) return response.text();
 
     if (response.status >= 400)
       throw {
@@ -84,33 +90,33 @@ export class createClient {
         body: {
           message: response.statusText
         }
-      }
+      };
 
-    const json = await response.json()
+    const json = await response.json();
 
     if (!response.ok) {
       throw {
         statusCode: response.status,
         ...json
-      }
+      };
     }
 
-    return json
+    return json;
   }
 
   post(path: string, data: object, headers?: Headers) {
-    return this.request('POST', path, data, headers)
+    return this.request('POST', path, data, headers);
   }
 
   get(path: string, headers?: Headers) {
-    return this.request('GET', path, undefined, headers)
+    return this.request('GET', path, undefined, headers);
   }
 
   put(path: string, data: object, headers?: Headers) {
-    return this.request('PUT', path, data, headers)
+    return this.request('PUT', path, data, headers);
   }
-  
+
   delete(path: string, data: object, headers?: Headers) {
-    return this.request('DELETE', path, data, headers)
+    return this.request('DELETE', path, data, headers);
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,25 @@
 export interface InitOptions {
-  store_name: string,
-  access_token?: string,
-  client_key?: string,
-  client_pass?: string,
-  fetch?: Fetch
-  application?: string
-  headers?: Headers
+  store_name: string;
+  admin_access_token?: string;
+  storefront_access_token?: string;
+  client_key?: string;
+  client_pass?: string;
+  fetch?: Fetch;
+  application?: string;
+  headers?: Headers;
 }
 
 export interface Options {
-  host: string
-  fetch?: Fetch
-  application?: string
-  headers?: Headers
+  host: string;
+  fetch?: Fetch;
+  application?: string;
+  headers?: Headers;
 }
 
 export interface Headers {
-  [key: string]: string
+  [key: string]: string;
 }
 
 export interface Fetch {
-  (input?: Request | string, init?: RequestInit): Promise<Response>
+  (input?: Request | string, init?: RequestInit): Promise<Response>;
 }
